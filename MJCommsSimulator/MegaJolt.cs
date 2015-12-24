1a81f78ae8f84ef8ce04a56b7c61355f4cfce2bb
Robert Barnes <robertbarnes1981@googlemail.com>
2015-12-24 16:40:15 +0000

    Handle version request, slower cycle ignition advance

--- MJCommsSimulator/MegaJolt.cs
+++ MJCommsSimulator/MegaJolt.cs
@@ -7,10 +7,14 @@ namespace MJCommsSimulator
     {
         private SerialPort m_port;
 
+        DateTime m_startTime;
+
         private byte m_advanceDegrees = 0x00;
 
         public MegaJolt(string port)
         {
+            m_startTime = DateTime.Now;
+
             m_port = new SerialPort(port, 38400, Parity.None, 8, StopBits.One);
 
             m_port.DataReceived += port_dataReceived;
@@ -30,11 +34,23 @@ namespace MJCommsSimulator
             {
                 switch ((char)buffer)
                 {
+                    case 'V':
+                        Console.WriteLine("{0} VERSION Request 'V'", DateTime.Now);
+
+                        byte[] v_bytes = new Version().ToBytes();
+                        m_port.Write(v_bytes, 0, v_bytes.Length);
+                        break;
+
                     case 'S':
-                        byte[] data = new State() { advanceDegrees = m_advanceDegrees }.ToBytes();
-                        m_advanceDegrees++;
-                        Console.WriteLine("{0} STATUS Request 'S'", DateTime.Now);
-                        m_port.Write(data, 0, data.Length);
+                        Console.WriteLine("{0} STATUS Request 'S' {1}", DateTime.Now, m_advanceDegrees);
+
+                        byte[] s_bytes = new State() { advanceDegrees = m_advanceDegrees }.ToBytes();
+                        if ((DateTime.Now - m_startTime).TotalMilliseconds > 125)
+                        {
+                            m_advanceDegrees++;
+                            m_startTime = DateTime.Now;
+                        }
+                        m_port.Write(s_bytes, 0, s_bytes.Length);
                         break;
 
                     default:
---
+++ MJCommsSimulator/Version.cs
@@ -0,0 +1,19 @@
+﻿namespace MJCommsSimulator
+{
+    class Version
+    {
+        public byte major;
+        public byte minor;
+        public byte bugfix;
+
+        public byte[] ToBytes()
+        {
+            return new byte[]
+            {
+                major,
+                minor,
+                bugfix
+            };
+        }
+    }
+}
